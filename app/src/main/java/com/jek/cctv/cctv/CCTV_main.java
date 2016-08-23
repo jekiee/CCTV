1305faa1fadc21956df529f455793ecc0fcdda68
jekiee <jekiee007@gmail.com>
2016-08-23 20:23:20 +0300

    1st commit

---
+++ app/src/main/java/com/jek/cctv/cctv/CCTV_main.java
@@ -0,0 +1,13 @@
+package com.jek.cctv.cctv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CCTV_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cctv_main);
+    }
+}
